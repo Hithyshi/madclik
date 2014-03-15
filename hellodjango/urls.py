8b3efc4c08a1b0f48ef93e208872151ddea621cc
Hithyshi Krishnamurthy <hithyshikrishnamurthy@Hithyshis-MacBook-Pro.local>
2014-03-15 17:47:07 +0530

    checking homepage

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^zone/$', 'zone.views.homepage', name='homepage'),
+    url(r'^zone/search/$', 'zone.views.homesearch', name='homesearch'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- zone/views.py
+++ zone/views.py
@@ -6,3 +6,6 @@ from django.template import RequestContext
 
 def homepage(request):
   return render_to_response("homepage.html",context_instance=RequestContext(request)) 
+
+def homesearch(request):
+  return render_to_response("searchhome.html", context_instance=RequestContext(request))
